f881aa070ca747b6a7d0c02bcbffa7964b54cb5c
MuhammadImran01 <Muhammad.Imran@symmetrygroup.biz>
2019-09-13 11:45:00 +0500

    Implement Notification with Update Broadcast

---
+++ app/src/main/java/com/example/roomlivedataapp/Utils/AppNotifications.java
@@ -0,0 +1,87 @@
+package com.example.roomlivedataapp.Utils;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.roomlivedataapp.R;
+import com.example.roomlivedataapp.ui.mainScreen.MainActivity;
+
+public class AppNotifications {
+    // Constants for the notification actions buttons.
+    public static final String ACTION_UPDATE_NOTIFICATION =
+            "com.android.example.notifyme.ACTION_UPDATE_NOTIFICATION";
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private static final int NOTIFICATION_ID = 0;
+    private NotificationManager mNotifyManager;
+
+    public AppNotifications(Context context) {
+        mNotifyManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            // Create a NotificationChannel
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Test Notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Android");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(Context context, String Title, String Text) {
+        // Set up the pending intent that is delivered when the notification
+        // is clicked.
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, notificationIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat
+                .Builder(context, PRIMARY_CHANNEL_ID)
+                .setContentTitle(Title)
+                .setContentText(Text)
+                .setAutoCancel(true).setContentIntent(notificationPendingIntent)
+                .setSmallIcon(R.drawable.ic_launcher_background);
+        return notifyBuilder;
+    }
+
+    public void SendNotification(Context context) {
+        // Sets up the pending intent to update the notification.
+        // Corresponds to a press of the Update Me! button.
+        Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast(context,
+                NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        // Build the notification with all of the parameters using helper
+        // method.
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder(context,
+                "Rood Data Notification", "Welcome To Notification");
+
+        // Add the action button using the pending intent.
+        notifyBuilder.addAction(R.drawable.ic_launcher_background,
+                context.getString(R.string.update), updatePendingIntent);
+
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
+    public void updateNotification(Context context) {
+        // Build the notification with all of the parameters using helper
+        // method.
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder(context,
+                "Room Notification Updated!!!", "Notification Updated");
+
+        // Deliver the notification.
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+
+    }
+}
--- app/src/main/java/com/example/roomlivedataapp/ui/mainScreen/MainActivity.java
+++ app/src/main/java/com/example/roomlivedataapp/ui/mainScreen/MainActivity.java
@@ -7,12 +7,17 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.roomlivedataapp.R;
+import com.example.roomlivedataapp.Utils.AppNotifications;
 import com.example.roomlivedataapp.viewModel.WordViewModel;
 import com.example.roomlivedataapp.adapters.WordListAdapter;
 import com.example.roomlivedataapp.databinding.ActivityMainBinding;
@@ -22,11 +27,15 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static com.example.roomlivedataapp.Utils.AppNotifications.ACTION_UPDATE_NOTIFICATION;
+
 public class MainActivity extends AppCompatActivity implements ClickListeners {
     private RecyclerView recyclerView;
     private WordListAdapter adapter;
     private WordViewModel viewModel;
     private ActivityMainBinding binding;
+    private NotificationReceiver mReceiver = new NotificationReceiver();
+    AppNotifications appNotifications;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +62,10 @@ public class MainActivity extends AppCompatActivity implements ClickListeners {
     }
 
     private void init() {
+        // Register the broadcast receiver to receive the update action from
+        // the notification.
+        registerReceiver(mReceiver,
+                new IntentFilter(ACTION_UPDATE_NOTIFICATION));
         recyclerView = findViewById(R.id.recyclerview);
         adapter = new WordListAdapter(this);
         binding.setAdapter(adapter);
@@ -65,5 +78,38 @@ public class MainActivity extends AppCompatActivity implements ClickListeners {
     public void onFabClick() {
         viewModel.insertWord(new Word("Hello 8"+i));
         i++;
+        appNotifications = new AppNotifications(this);
+        appNotifications.SendNotification(this);
+    }
+
+    /**
+     * Unregisters the receiver when the app is being destroyed.
+     */
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+    /**
+     * The broadcast receiver class for notifications.
+     * Responds to the update notification pending intent action.
+     */
+    public class NotificationReceiver extends BroadcastReceiver {
+
+        public NotificationReceiver() {
+        }
+
+        /**
+         * Receives the incoming broadcasts and responds accordingly.
+         *
+         * @param context Context of the app when the broadcast is received.
+         * @param intent The broadcast intent containing the action.
+         */
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Update the notification.
+            appNotifications.updateNotification(context);
+        }
     }
 }
